7b1d0d97393e931d65ad860f1880aa349498e180
William Jones <williamseejones793@gmail.com>
2022-01-07 15:03:18 -0500

    Added phx_gen_tailwind dependance 0.1.3

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Buzzed.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:phx_gen_tailwind, "~> 0.1.3"}
     ]
   end
 
@@ -61,11 +62,11 @@ defmodule Buzzed.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
+      setup: ["deps.get", "ecto.setup", "cmd --cd assets npm install"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": [
+      "assets.deploy": ["cmd --cd assets npm run deploy", 
         "cmd --cd assets npm run deploy",
         "esbuild default --minify",
         "phx.digest"
